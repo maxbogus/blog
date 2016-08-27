6f90cc6228cbf51063eb48b70b2910ae6b8cffb2
Max Boguslavskiy <max.bogus@gmail.com>
2016-08-28 00:28:04 +0300

    - checked auth; - added comments to code.

--- main.py
+++ main.py
@@ -68,10 +68,13 @@ def valid_cookie(cookie):
 
 # main handler function
 class Handler(webapp2.RequestHandler):
+    """ Main handler with main methods."""
+
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
+        """ Render template with default params."""
         t = jinja_env.get_template(template)
         params['logged'] = self.read_secure_cookie('user_id')
         params["user"] = self.user
@@ -81,6 +84,7 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
     def set_secure_cookie(self, name, val):
+        """ Set cookie with encrypted value."""
         cookie_val = make_secure_val(val)
         value = '%s=%s; Path=/' % (name, cookie_val)
         self.response.headers.add_header('Set-Cookie', value)
@@ -90,14 +94,17 @@ class Handler(webapp2.RequestHandler):
         return cookie_val and check_secure_val(cookie_val)
 
     def login(self, user):
+        """ Set id and login cookies."""
         self.set_secure_cookie('user_id', str(user.key().id()))
         self.set_secure_cookie('user_login', str(user.name))
 
     def logout(self):
+        """ Remove values from id and login cookies."""
         self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
         self.response.headers.add_header('Set-Cookie', 'user_login=; Path=/')
 
     def initialize(self, *a, **kw):
+        """ Initialize request. Read cookie and check user_id in db."""
         webapp2.RequestHandler.initialize(self, *a, **kw)
         uid = self.read_secure_cookie('user_id')
         self.user = uid and dbmodel.User.by_id(int(uid))
@@ -105,10 +112,17 @@ class Handler(webapp2.RequestHandler):
 
 # Authentication section
 class SignupPage(Handler):
+    """ Signup page handler"""
+
     def get(self):
+        """ Render form"""
         self.render("signup.html")
 
     def post(self):
+        """ Get data from form. By default we assume that we have no error.
+        Then we verify data. According to false data we add errors to params
+        dict. Then we check if user exist. If not we add it. If exist
+        we show error."""
         have_error = False
         form_username = self.request.get('username')
         form_password = self.request.get('password')
@@ -152,7 +166,11 @@ class SignupPage(Handler):
 
 
 class MainPage(Handler):
+    """ Main page handler"""
+
     def render_front(self, subject='', content='', error=''):
+        """ Query to get all posts ordered by created is provided.
+        Then page is rendered with query result."""
         query = 'SELECT * FROM Blog ORDER BY created DESC'
         posts = dbmodel.db.GqlQuery(query)
         self.render("blog.html",
@@ -163,7 +181,10 @@ class MainPage(Handler):
 
 
 class WelcomeHandler(Handler):
+    """ Welcome page handler"""
+
     def get(self):
+        """ Check auth. If not redirect to signup."""
         if self.user:
             self.render("success.html", username=self.user.name)
         else:
@@ -171,10 +192,15 @@ class WelcomeHandler(Handler):
 
 
 class LoginPage(Handler):
+    """ Login page handler"""
+
     def get(self):
+        """ Render page"""
         self.render("login.html")
 
     def post(self):
+        """ Get data from user. Try to get user from db.
+        If exist - redirect to welcome. If not - show error."""
         username = self.request.get('username')
         password = self.request.get('password')
 
@@ -188,24 +214,36 @@ class LoginPage(Handler):
 
 
 class LogoutPage(Handler):
+    """ Logout page handler"""
+
     def get(self):
+        """ Delete user cookies and redirect to signup."""
         self.logout()
         self.redirect("/blog/signup")
 
 
 class ErrorPageHandler(Handler):
+    """ Error page handler"""
+
     def get(self, error_type):
+        """ Error type is got from path.
+        Render form."""
         self.render("error.html", error_type=error_type)
 
 
 class NewPostHandler(Handler):
+    """ New post page handler"""
+
     def get(self):
+        """ Auth required for new posts. If not authed - redirect to login."""
         if self.user:
             self.render("newpost.html")
         else:
             self.redirect('/blog/login')
 
     def post(self):
+        """ Auth required for new posts. If not authed - redirect to login.
+        Then check fields from user. If data is incomplete - show error."""
         if self.user:
             subject = self.request.get('subject')
             content = self.request.get('content')
@@ -269,19 +307,25 @@ class PostEditHandler(Handler):
             self.redirect('/blog/login')
 
     def post(self, post_id):
-        post = dbmodel.Blog.get_by_id(int(post_id))
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-        if subject and content:
-            post.subject = subject
-            post.content = content
-            post.put()
+        """ Check auth. Show error if auth is incorrect.
+        Get subject and content. If any put data in base.
+        If not show error."""
+        if self.user:
+            post = dbmodel.Blog.get_by_id(int(post_id))
+            subject = self.request.get('subject')
+            content = self.request.get('content')
+            if subject and content:
+                post.subject = subject
+                post.content = content
+                post.put()
 
-            self.redirect("/blog/%s" % post.key().id())
+                self.redirect("/blog/%s" % post.key().id())
+            else:
+                error = "Subject and content are required"
+                self.render("newpost.html",
+                            subject=subject, content=content, error=error)
         else:
-            error = "Subject and content are required"
-            self.render("newpost.html",
-                        subject=subject, content=content, error=error)
+            self.redirect('/blog/login')
 
 
 class PostDeleteHandler(Handler):
@@ -334,40 +378,59 @@ class PostLikeHandler(Handler):
 
 
 class NewComment(Handler):
+    """ New comment handler."""
+
     def get(self, post_id):
+        """ To add new comment auth is required. If
+        not authorized - send user to login.
+        Otherwise render form. Check that post
+        exists in data base."""
         if self.user:
             post = dbmodel.Blog.get_by_id(int(post_id))
-            subject = post.subject
-            content = post.content
-            self.render("newcomment.html",
-                        subject=subject,
-                        content=content,
-                        pkey=post.key())
+            if not post:
+                error = 404
+                self.error(error)
+                self.render('error.html', error=error)
+            else:
+                subject = post.subject
+                content = post.content
+                self.render("newcomment.html",
+                            subject=subject,
+                            content=content,
+                            pkey=post.key())
         else:
             self.redirect('/blog/login')
 
     def post(self, post_id):
-        post = dbmodel.Blog.get_by_id(int(post_id))
-        if not post:
-            error = 404
-            self.error(error)
-            self.render('error.html', error=error)
-        else:
-            comment = self.request.get("comment")
-            author = self.user.name
-
-            if comment:
-                c = dbmodel.Comment(comment=comment,
-                                    post=post_id,
-                                    parent=self.user.key(),
-                                    author=author)
-                c.put()
-                self.redirect("/blog")
+        """ To add new comment auth is required. If
+        not authorized - send user to login.
+        Otherwise render form. If post is not existed -
+        render error. Check entered data if it's
+        incomplete - show error."""
+        if self.user:
+            post = dbmodel.Blog.get_by_id(int(post_id))
+            if not post:
+                error = 404
+                self.error(error)
+                self.render('error.html', error=error)
             else:
-                error = "please comment"
-                self.render("post.html",
-                            post=post,
-                            error=error)
+                comment = self.request.get("comment")
+                author = self.user.name
+
+                if comment:
+                    c = dbmodel.Comment(comment=comment,
+                                        post=post_id,
+                                        parent=self.user.key(),
+                                        author=author)
+                    c.put()
+                    self.redirect("/blog")
+                else:
+                    error = "please comment"
+                    self.render("post.html",
+                                post=post,
+                                error=error)
+        else:
+            self.redirect('/blog/login')
 
 
 class EditComment(Handler):
@@ -396,19 +459,25 @@ class EditComment(Handler):
             self.redirect('/blog/login')
 
     def post(self, post_id, comment_id):
-        comment = dbmodel.Comment.get_by_id(int(comment_id),
-                                            parent=self.user.key())
-        post = dbmodel.Blog.get_by_id(int(post_id))
-        comment_edit = self.request.get("comment")
-        if comment_edit:
-            comment.comment = comment_edit
-            comment.put()
-            self.redirect("/blog/%s" % str(post_id))
+        """ If user is authorized get user data. Check edits
+        and if they exist - add them to base. Otherwise
+        ask to add comment."""
+        if self.user:
+            comment = dbmodel.Comment.get_by_id(int(comment_id),
+                                                parent=self.user.key())
+            post = dbmodel.Blog.get_by_id(int(post_id))
+            comment_edit = self.request.get("comment")
+            if comment_edit:
+                comment.comment = comment_edit
+                comment.put()
+                self.redirect("/blog/%s" % str(post_id))
+            else:
+                error = "Please fill in comment."
+                self.render("newcomment.html", subject=post.subject,
+                            content=post.content, comment=comment.comment,
+                            error=error, pkey=post.key())
         else:
-            error = "Please fill in comment."
-            self.render("newcomment.html", subject=post.subject,
-                        content=post.content, comment=comment.comment,
-                        error=error, pkey=post.key())
+            self.redirect('/blog/login')
 
 
 class DeleteComment(Handler):
